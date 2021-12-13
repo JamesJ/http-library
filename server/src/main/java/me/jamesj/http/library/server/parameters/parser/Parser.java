e12bffaa1e7157711f120bd8a48e948e0192b8cb
James <contact@jamesj.me>
2021-12-13 20:55:23 +0000

    fix import and clean up code
    Signed-off-by: James <contact@jamesj.me>

--- server/src/main/java/me/jamesj/http/library/server/parameters/parser/Parser.java
+++ server/src/main/java/me/jamesj/http/library/server/parameters/parser/Parser.java
@@ -9,79 +9,71 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 
 public interface Parser<T> {
-    
+
     @NotNull
     static StringParser<String> asString() {
         return str -> str;
     }
-    
+
     @NotNull
     static StringParser<Number> asNumber() {
-        return str -> NumberUtils.createNumber(str);
+        return NumberUtils::createNumber;
     }
-    
+
     @NotNull
     static StringParser<Integer> asInteger() {
-        return str -> NumberUtils.createInteger(str);
+        return NumberUtils::createInteger;
     }
-    
+
     @NotNull
     static StringParser<Double> asDouble() {
-        return str -> NumberUtils.createDouble(str);
+        return NumberUtils::createDouble;
     }
-    
+
     @NotNull
     static StringParser<Long> asLong() {
-        return str -> NumberUtils.createLong(str);
+        return NumberUtils::createLong;
     }
-    
+
     @NotNull
     static StringParser<Float> asFloat() {
-        return str -> NumberUtils.createFloat(str);
+        return NumberUtils::createFloat;
     }
-    
+
     @NotNull
     static StringParser<Byte> asByte() {
-        return str -> NumberUtils.toByte(str);
+        return NumberUtils::toByte;
     }
-    
+
     @NotNull
     static StringParser<Boolean> asBoolean() {
         return (str) -> {
             if (str.equalsIgnoreCase("yes")
-                || str.equalsIgnoreCase("y")
-                || str.equalsIgnoreCase("t")
-                || str.equalsIgnoreCase("1")) {
+                    || str.equalsIgnoreCase("y")
+                    || str.equalsIgnoreCase("t")
+                    || str.equalsIgnoreCase("1")) {
                 return true;
             }
             return asStrictBoolean().parse(str);
         };
     }
-    
+
     @NotNull
     static StringParser<Boolean> asStrictBoolean() {
-        return str -> {
-            return str.equalsIgnoreCase("true");
-        };
+        return str -> str.equalsIgnoreCase("true");
     }
-    
+
     @NotNull
     static Parser<File> asFile() {
-        return new Parser<>() {
-            @Override
-            public @Nullable File parse(@NotNull Object data, Map<String, String> metadata) {
-                if (data instanceof byte[]) {
-                    MediaType mediaType = MediaType.parse(metadata.get("content-type"));
-                    if (mediaType == null) {
-                        return null;
-                    }
-                    return new File.FileImpl(metadata.get("name"), mediaType, (byte[]) data);
-                }
-                return null;
+        return (data, metadata) -> {
+            if (data instanceof byte[]) {
+                MediaType mediaType = MediaType.parse(metadata.get("content-type"));
+                return new File.FileImpl(metadata.get("name"), mediaType, (byte[]) data);
             }
+            return null;
         };
     }
-    
+
     @Nullable T parse(@NotNull Object data, Map<String, String> metadata);
-    
+
 }
