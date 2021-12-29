9b3f58f334bdddb9591ee36b148b342e9cf90fdc
JamesJ <contact@jamesj.me>
2021-12-29 11:30:15 +0000

    regex validator

--- server/src/main/java/me/jamesj/http/library/server/parameters/Validator.java
+++ server/src/main/java/me/jamesj/http/library/server/parameters/Validator.java
@@ -13,31 +13,36 @@ import org.slf4j.LoggerFactory;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("UnstableApiUsage")
 public interface Validator<T> {
-
+    
     static <T> Validator<T> min(int length) {
         return new MinimumLengthValidator(length);
     }
-
+    
     static <T> Validator<T> max(int length) {
         return new MaximumLengthValidator(length);
     }
-
+    
     static Validator<File> type(MediaType... types) {
         return new MediaTypeValidator(types);
     }
-
+    
+    static Validator<String> regex(Pattern pattern) {
+        return new RegexValidator(pattern);
+    }
+    
     @Nullable
     Failure test(@Nullable T t);
-
+    
     class MinimumLengthValidator implements Validator {
-
+        
         private final int value;
         private final String failureMessage;
-
+        
         /**
          * @param value          - the desired minimum length
          * @param failureMessage - the failure message, can use the parameters  minimum length and current length via %s
@@ -46,14 +51,14 @@ public interface Validator<T> {
             this.value = value;
             this.failureMessage = failureMessage;
         }
-
+        
         /**
          * @param value - the desired minimum length
          */
         public MinimumLengthValidator(int value) {
             this(value, "Value is too short, must be a minimum of %s (current: %s)");
         }
-
+        
         @Override
         public @Nullable Failure test(@Nullable Object o) {
             if (o instanceof Number) {
@@ -77,12 +82,12 @@ public interface Validator<T> {
             return null;
         }
     }
-
+    
     class MaximumLengthValidator implements Validator {
-
+        
         private final int value;
         private final String failureMessage;
-
+        
         /**
          * @param value          - the desired maximum length
          * @param failureMessage - the failure message, can use the parameters maximum length and current length via %s
@@ -91,14 +96,14 @@ public interface Validator<T> {
             this.value = value;
             this.failureMessage = failureMessage;
         }
-
+        
         /**
          * @param value - the desired maximum length
          */
         public MaximumLengthValidator(int value) {
             this(value, "Value is too long, must be a maximum of %s (current: %s)");
         }
-
+        
         @Override
         public @Nullable Failure test(@Nullable Object o) {
             if (o instanceof Number) {
@@ -122,12 +127,12 @@ public interface Validator<T> {
             return null;
         }
     }
-
+    
     class MediaTypeValidator implements Validator<File> {
-
+        
         private final MediaType[] value;
         private final String failureMessage;
-
+        
         /**
          * @param value          - the desired maximum length
          * @param failureMessage - the failure message, can use the parameters allowed and current via %s
@@ -136,14 +141,14 @@ public interface Validator<T> {
             this.value = value;
             this.failureMessage = failureMessage;
         }
-
+        
         /**
          * @param value - the desired maximum length
          */
         public MediaTypeValidator(MediaType[] value) {
             this(value, "Media type is not allowed, only accepts %s (current: %s)");
         }
-
+        
         @Override
         public @Nullable Failure test(@Nullable File file) {
             for (MediaType mediaType : value) {
@@ -153,65 +158,82 @@ public interface Validator<T> {
             }
             return Failure.of(String.format(failureMessage, allowed(), file.mediaType().toString()));
         }
-
+        
         private String allowed() {
             return Arrays.stream(value).map(MediaType::toString).collect(Collectors.joining(","));
         }
     }
-
+    
+    class RegexValidator implements Validator<String> {
+        
+        private final Pattern pattern;
+        
+        public RegexValidator(Pattern pattern) {
+            this.pattern = pattern;
+        }
+        
+        @Override
+        public @Nullable Failure test(@Nullable String s) {
+            if (pattern.matcher(s).matches()) {
+                return null;
+            }
+            return Failure.of("String \"" + s + "\" does not match regex \"" + pattern.pattern() + "\"");
+        }
+    }
+    
     class Failure {
-
+        
         private final String message;
-
+        
         public Failure(String message) {
             this.message = message;
         }
-
+        
         public static Failure of(String reason) {
             return new Failure(reason);
         }
-
+        
         public String getMessage() {
             return message;
         }
     }
-
+    
     class ValidatorFilter implements HttpFilter {
-
+        
         private final Logger logger;
         private final Parameter<?>[] parameters;
-
+        
         public ValidatorFilter(Parameter<?>[] parameters) {
             this.logger = LoggerFactory.getLogger(getClass());
             this.parameters = parameters;
         }
-
+        
         @Override
         public @NotNull Logger getLogger() {
             return this.logger;
         }
-
-        @SuppressWarnings({"rawtypes", "unchecked"})
+        
+        @SuppressWarnings({ "rawtypes", "unchecked" })
         @Override
         public @NotNull CompletableFuture<@Nullable Void> filter(@NotNull HttpRequest httpRequest) {
             Map<Parameter<?>, Failure[]> failureMap = new HashMap<>();
-
+            
             for (Parameter parameter : parameters) {
                 List<Failure> failures = new ArrayList<>();
                 List<Validator> validators = parameter.validators();
-
+                
                 Object obj = null;
                 try {
                     obj = parameter.fetch(httpRequest);
                 } catch (ParsingException e) {
                     failures.add(e.getFailure());
                 }
-
+                
                 if (obj == null && parameter.required()) {
                     failures.add(Failure.of("Value not provided"));
                     continue;
                 }
-
+                
                 if (validators != null) {
                     for (Validator validator : validators) {
                         Failure failure = validator.test(obj);
@@ -220,12 +242,12 @@ public interface Validator<T> {
                         }
                     }
                 }
-
+                
                 if (!failures.isEmpty()) {
                     failureMap.put(parameter, failures.toArray(Failure[]::new));
                 }
             }
-
+            
             if (failureMap.isEmpty()) {
                 return CompletableFuture.completedFuture(null);
             }
